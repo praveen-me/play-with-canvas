dd5805d084671adcf073c97dfa83767f8b6bb5bc
praveen-me <praveenkrsaini0@gmail.com>
2018-10-04 23:33:02 +0530

    Added README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# Play With Canvas 
+This repository contains some cool stuff doing using canvas.
+
+## Demo of topics
+Click on particular topic to open it's demo.
+
+* [Canvas Resize](https://praveen-me.github.io/play-with-canvas/canvas-resize)
+
+* [Animate Canvas](https://praveen-me.github.io/play-with-canvas/animate-canvas)
+
+*  [Code Gravity](https://praveen-me.github.io/play-with-canvas/code-gravity)
+
+*  [Collison Detection](https://praveen-me.github.io/play-with-canvas/collision-detection)
--- collision-detection/canvas.js
+++ collision-detection/canvas.js
@@ -3,8 +3,8 @@ import utils from './utils'
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 const mouse = {
     x: innerWidth / 2,
@@ -15,45 +15,59 @@ const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 
 // Event Listeners
 addEventListener('mousemove', event => {
-    mouse.x = event.clientX
-    mouse.y = event.clientY
-})
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+});
 
 addEventListener('resize', () => {
     canvas.width = innerWidth
-    canvas.height = innerHeight
+    canvas.height = innerHeight;
 
-    init()
-})
+    init();
+});
 
-// Objects
-function Object(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
+class Ball {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+        c.closePath()
+    }
+
+    update() {
+        this.draw()
+    }
+    
 }
 
-Object.prototype.draw = function() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-}
+const getDistance = (x1, x2, y1, y2) => {
+    let xDistance = x1 - x2;
+    let yDistance = y1 - y2;
 
-Object.prototype.update = function() {
-    this.draw()
+    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
 }
 
 // Implementation
 let objects
+let ball;
+let ball2;
 function init() {
     objects = []
 
     for (let i = 0; i < 400; i++) {
         // objects.push();
     }
+
+    ball = new Ball(canvas.width / 2, canvas.height / 2, 100, 'black');
+    ball2 = new Ball(10, 10, 30, 'red');
 }
 
 // Animation Loop
@@ -61,10 +75,20 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
 
-    c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-    // objects.forEach(object => {
-    //  object.update();
-    // });
+    let distance = getDistance(ball.x, ball2.x, ball.y, ball2.y);
+
+
+    if(distance < ball.radius + ball2.radius) {
+        ball.color = 'red';
+    } else {
+        ball.color = 'black';
+    }
+
+    ball.update();
+    ball2.update()
+    ball2.x = mouse.x;
+    ball2.y = mouse.y;
+
 }
 
 init()
